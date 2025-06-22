e69ddee1fb17e853132a1e2a4d56807e4af2cf42
Maor Bar <86046742+maor224@users.noreply.github.com>
2025-06-22 15:57:18 +0300

    Update dependencies and Python version constraints in pyproject.toml
    - Changed Python version requirement to be less than 4.0.0.
    - Updated alpaca-trade-api dependency to alpaca-py with a version constraint.
    - Ensured compatibility of FastAPI, Uvicorn, and Pydantic settings versions.

--- app/models/trade_request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-from app.models.enums import TradeSide
-
-
-class TradeRequest(BaseModel):
-    symbol: str = Field(..., example="AAPL")
-    qty: int = Field(..., gt=0, example=10)
-    side: TradeSide = Field(..., example="buy")
---
+++ app/routes/order.py
@@ -0,0 +1,30 @@
+import traceback
+
+from alpaca.trading.requests import LimitOrderRequest, MarketOrderRequest
+from fastapi import APIRouter, Depends
+
+from app.utils.alpaca_client import get_alpaca_client
+
+router = APIRouter()
+
+
+@router.post("/market")
+def market_order(
+    market_order_request: MarketOrderRequest, alpaca_client=Depends(get_alpaca_client)
+):
+    try:
+        order = alpaca_client.submit_order(order_data=market_order_request)
+        return {"message": "Order placed successfully", "order": order._raw}
+    except Exception as e:
+        return {"error": str(e), "traceback": traceback.format_exc()}
+
+
+@router.post("/limit")
+def limit_order(
+    limit_order_request: LimitOrderRequest, alpaca_client=Depends(get_alpaca_client)
+):
+    try:
+        order = alpaca_client.submit_order(order_data=limit_order_request)
+        return {"message": "Order placed successfully", "order": order._raw}
+    except Exception as e:
+        return {"error": str(e), "traceback": traceback.format_exc()}
